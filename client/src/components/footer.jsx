f8569115b92e0f1d252824d303578f236d5420a6
heyyrahul <rkkamat9213@gmail.com>
2024-05-09 23:02:55 +0530

    footer corrected

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,49 +1,76 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-
+import travel2 from '../assets/travel2.png';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import  EmailIcon from '@mui/icons-material/Email';
 const Footer = () => {
   return (
     <div style={{ backgroundColor: '#333' }}>
       <footer style={{ backgroundColor: '#333', color: '#fff', padding: '20px', display: 'flex', justifyContent: 'space-between', gap: '20px', marginBottom: '0px', width: '100%' }}>
         <div>
-          <Typography variant="h6">
-            Travelopia
-          </Typography>
+          <Link to="/" style={{ textDecoration: 'none' }}>  
+            <img src={travel2} alt="" style={{ width: '120px', height: '70px', margin: '-10px 10px' }} />
+          </Link>
           <Typography variant="subtitle1">
             Your ultimate travel companion
           </Typography>
+          <Typography  variant="body2" color="inherit">
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}><FacebookIcon /> </Link>
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}><TwitterIcon /></Link>
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}><InstagramIcon /></Link>
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}><EmailIcon /></Link>
+          </Typography> 
         </div>
         <div>
-          <Typography variant="body2" color="inherit">
-            © {new Date().getFullYear()} Travelopia. All rights reserved.
+          <Typography    variant="body2" style={{ fontSize: '20px', color: '#DC5431' }}>
+            Quick Links
           </Typography>
           <Typography variant="body2" color="inherit">
-            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Privacy Policy</Link>
-            |
-            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Terms of Use</Link>
-            |
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Services</Link>
+          </Typography>
+          <Typography variant="body2" color="inherit">
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>About Us</Link>
+          </Typography>
+          <Typography variant="body2" color="inherit">
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Career</Link>
+          </Typography>
+          <Typography variant="body2" color="inherit">
             <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Contact Us</Link>
           </Typography>
         </div>
         <div>
-          <Typography variant="body2" color="inherit">
-            Follow us:
+          <Typography variant="body2" style={{ fontSize: '20px', color: '#DC5431' }}>
+        Our Brands
           </Typography>
-          <Typography variant="body2" color="inherit">
-            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Facebook</Link>
-            |
-            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Twitter</Link>
-            |
-            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Instagram</Link>
+          <Typography  variant="body2" color="inherit">
+          <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>Quark Expeditions</Link>
+          </Typography>
+          <Typography  variant="body2" color="inherit">
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>  TCS World Travel</Link>
+          </Typography>
+          <Typography  variant="body2" color="inherit">
+            <Link href="#" style={{ color: '#fff', textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>  Kalos Golf</Link>
+          </Typography>
+        </div>
+        <div>
+        <Typography variant="body2"  style={{ fontSize: '20px', color: '#DC5431' }}>
+          Address:
+          </Typography>
+          <Typography>
+          123 Main Street, Anytown,
+          </Typography>
+          <Typography>
+          USA
           </Typography>
         </div>
       </footer>
-      <hr style={{ margin: '10px 0', width: '100%', border: '1px solid #fff' }} />
+      <hr style={{ marginTop: '0px', width: '100%', border: '1px solid #fff' }} />
       <Typography variant="body2" color="inherit" style={{ textAlign: 'center' }}>
-           © 2024 Travelopia | Designed by <a href="https://heyyrahul.github.io/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'none' }}><strong style={{ color: '#DC5431' }}>Rahul Kumar</strong></a>
-            </Typography>
-
+        © {new Date().getFullYear()} Travelopia | Designed by <a href="https://heyyrahul.github.io/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'none' }}><strong style={{ color: '#DC5431' }}>Rahul Kumar</strong></a> 
+      </Typography>
     </div>
   );
 };
